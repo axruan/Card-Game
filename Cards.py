0afe4a91aeb2d659387c04fba76289555a93d2d8
axruan <42079946+axruan@users.noreply.github.com>
2018-11-02 15:20:34 -0400

    Update Cards.py

--- Cards.py
+++ Cards.py
@@ -1,24 +1,40 @@
 nums = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
 suites = ["Hearts", "Spades", "Clubs", "Diamonds"]
 
+
 class Cards:
     def __init__(self, num, suite):
         self.n = num
         self.s = suite
 
-    def show(self):
-        print("The ", self.n, "of ", self.s)
+    def __str__(self):
+        return "The ", self.n, "of ", self.s
+
+
 
 
 class Deck:
     def __init__(self):
-        deck = []
-        for i in range(len(suites)):
-            for j in range(len(nums)):
-                deck.append(Cards(nums[j-1], suites[i-1]))
+        self.deck = []
 
     def __str__(self):
-        return
+        for i in range(len(self.deck)):
+            self.printer = self.printer + "\n" + str(self.deck[i])
+        return self.printer
+
+    def addCard(self, card):
+        self.deck.append(card)
+
+
+dealDeck = Deck()
+
+for i in range(len(suites)):
+    for j in range(len(nums)):
+        dealDeck.addCard(Cards(nums[j], suites[i]))
+
+x = str(dealDeck)
+
+
 
 
 
